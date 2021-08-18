d6e56df848b40908ba719ea61d2ce71fe1feb7ae
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2021-08-18 12:25:53 +0200

    added communication class to communicate with FPGA Master-EEA-SME/SailingBoatSimulatorFW#2

---
+++ src/Comm.py
@@ -0,0 +1,65 @@
+from serial import Serial
+
+class Comm():
+    __STARTCODE = 0x55
+    def __init__(self) -> None:
+        self.__Serial = Serial()
+    def setConfig(self, port : str, baud : int):
+        self.__Serial.port = port
+        self.__Serial.baudrate = baud
+        self.__Serial.bytesize = 8
+        self.__Serial.timeout = 1
+    def isOpen(self):
+        return self.__Serial.is_open
+    def open(self):
+        self.__Serial.open()
+    def close(self):
+        self.__Serial.close()
+    def __sendCmd(self, rw : bool, incr : bool, address : int, len : int):
+        if len != 0:
+            len -= 1
+            cmd = 0x1 if rw == True else 0x00
+            if incr == True: cmd |= 0x80
+            self.__Serial.write([Comm.__STARTCODE, cmd, address, len])
+    def __getAck(self) -> bool:
+        ack = self.__Serial.read(3)
+        return self.__parseData(ack, 1, True)
+
+    def writeAt(self, address : int, len : int, data : list, incr : bool = False) -> bool:
+        if len != 0:
+            self.__sendCmd(False, incr, address, len)
+            self.__Serial.write(data)
+            return self.__getAck()
+        return False
+
+    def readAt(self, address : int, xlen : int, incr : bool = False) -> list:
+        if xlen != 0:
+            self.__sendCmd(True, incr, address, xlen)
+            rxDat = self.__Serial.read(xlen + 2)
+            if self.__parseData(rxDat, xlen, False):
+                return list(rxDat[2:])
+            else:
+                return None
+#            print(len(rxDat))
+#            for i in range(0, len(rxDat) - 1, 16):
+#                print("0x{0:02X}".format(i), end='')
+#                for j in range(16):
+#                    if i + j >= len(rxDat):
+#                        break
+#                    print(" {0:02X}".format(rxDat[i + j]), end='')
+#                print()
+    @staticmethod
+    def __parseData(buf, expectedLen, isAck) -> bool:
+        expectedLen -= 1
+        for i in range(len(buf)):
+            if i == 0:
+                if buf[i] != Comm.__STARTCODE:
+                    return False
+            elif i == 1:
+                if buf[i] != expectedLen:
+                    return False
+            elif i == 2 and isAck:
+                if buf[i] != 1:
+                    return False
+        return True
+    
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,5 +1,6 @@
 from PySide6 import QtWidgets, QtCore
 from ui.MainWindowUI import Ui_MainWindow
+from Comm import Comm
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,8 +12,44 @@ class MainWindow(QtWidgets.QMainWindow):
         timer.timeout.connect(self.rotate)
         timer.start(100)
         self.rot = 0
-
+        self.ui.SimulateButton.clicked.connect(self.test)
+        self.__Comm = Comm()
+        self.__Comm.setConfig("COM5", 115200)
+        self.__Comm.open()
+        self.cnt = 0
+        self.ui.CompassACWButton.clicked.connect(self.minus)
+        self.ui.CompassCWButton.clicked.connect(self.plus)
     def rotate(self) -> None:
         self.ui.CompassWidget.rotate(self.rot)
         self.ui.WindWidget.rotate(self.rot)
         self.rot += 10
+    def minus(self) -> None:
+        for i in range(6):
+            self.__Comm.writeAt(0x80 + i*0x10, 2, int(500).to_bytes(2, 'little'), True)
+    def plus(self) -> None:
+        for i in range(6):
+            self.__Comm.writeAt(0x80 + i*0x10, 2, int(40000).to_bytes(2, 'little'), True)
+    def test(self) -> None:
+        dat = [0] * 16
+        for i in range(len(dat)):
+            temp = (self.cnt + i) % 256
+            dat[i] = temp
+        if not self.__Comm.writeAt(0x0, 16, dat, True):
+            print("Write Ko")
+        #else:
+        #    print("Write Ok")
+        rxDat = self.__Comm.readAt(0, 16, True)
+        if rxDat != None:
+            print("RxData :")
+            print("    ", end='')
+            for i in range(16):
+                print(" {0:02X}".format(i), end='')
+            print()
+            for i in range(0, len(rxDat) - 1, 16):
+                print("0x{0:02X}".format(i), end='')
+                for j in range(16):
+                    if i + j >= len(rxDat):
+                        break
+                    print(" {0:02X}".format(rxDat[i + j]), end='')
+                print()
+        self.cnt = (self.cnt + 1) % 256
